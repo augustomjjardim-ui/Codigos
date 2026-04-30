59af30a266f51ba987a603452eb92a730c66fee6
Augusto <augustomj.jardim@gmail.com>
2026-04-30 11:30:13 -0300

    comparação adicionada

--- 28-04-2026/Atividade/t.py
+++ 28-04-2026/Atividade/t.py
@@ -5,3 +5,8 @@ print(f'{round(num)}')
 lista=[67,94,3,95,4678]
 dobro=[v*2 for v in lista]
 print(dobro)
+#Comparação
+num1 =input("")
+num2 = input('')
+# Operador termario
+resultado = f"{num1} é maior." if num1 > num2 else f'{num2} é maior.'
