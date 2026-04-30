88cdbb6591b29dcb08a5d0339fec7d3d7728e90d
Augusto <augustomj.jardim@gmail.com>
2026-04-30 12:14:49 -0300

    Correção das atividades

--- 28-04-2026/Atividade/t.py
+++ 28-04-2026/Atividade/t.py
@@ -10,3 +10,23 @@ num1 =input("")
 num2 = input('')
 # Operador termario
 resultado = f"{num1} é maior." if num1 > num2 else f'{num2} é maior.'
+# Maipulação de lista
+nome1 = input('digite o primeiro nome completo: ')
+nome2 = input('digite o segundo nome completo: ')
+
+# Separando os dois
+parte1 = nome1.split()
+parte2 = nome2.split()
+
+#separando os nomes dentro das variaveis
+primeiro_nome = parte1[0]
+sobrenome1 = parte2[-1]
+
+segundo_nome = parte2[0]
+sobrenome2 = parte1[-1]
+
+novo_nome1 = primeiro_nome + " " + sobrenome1
+novo_nome2 = segundo_nome + " " + sobrenome2
+
+print(novo_nome1)
+print(novo_nome2)
